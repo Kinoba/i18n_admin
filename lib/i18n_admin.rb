a0e1336e480fe24d2dbb1feefc69bd03d2525a3e
Grégoire Willmann <5655900+GWillmann@users.noreply.github.com>
2024-12-21 23:41:52 +0100

    fix: translation pannel in bernardaud for rails 7

--- lib/i18n_admin.rb
+++ lib/i18n_admin.rb
@@ -11,17 +11,17 @@ require 'font-awesome-rails'
 
 require 'ext/paperclip'
 
-require "i18n_admin/request_store"
-require "i18n_admin/hstore_backend"
-require "i18n_admin/translations"
-require "i18n_admin/translation_collection"
-require "i18n_admin/translation"
+require 'i18n_admin/request_store'
+require 'i18n_admin/hstore_backend'
+require 'i18n_admin/translations'
+require 'i18n_admin/translation_collection'
+require 'i18n_admin/translation'
 
-require "i18n_admin/errors"
-require "i18n_admin/export"
-require "i18n_admin/import"
+require 'i18n_admin/errors'
+require 'i18n_admin/export'
+require 'i18n_admin/import'
 
-require "i18n_admin/engine"
+require 'i18n_admin/engine'
 
 module I18nAdmin
   mattr_accessor :root_controller_parent
@@ -42,9 +42,9 @@ module I18nAdmin
   mattr_accessor :async_io
   @@async_io = false
 
-  def self.config(&block)
+  def self.config
     block_given? ? yield(self) : self
   end
 end
 
-Mime::Type.register "application/xls", :xls
+Mime::Type.register 'application/xls', :xls
--- lib/i18n_admin/engine.rb
+++ lib/i18n_admin/engine.rb
@@ -3,12 +3,14 @@ module I18nAdmin
     isolate_namespace I18nAdmin
 
     initializer 'i18n_admin.configure_i18n_backend' do
-      translations_installed = begin
-        I18nAdmin::TranslationsSet.select('1').inspect
-        true
-      rescue
-        false
-      end
+      # TODO: remove this when we have a better way to check if the translations table exists
+      # begin
+      #   I18nAdmin::TranslationsSet.select('1').inspect
+      #   true
+      # rescue StandardError
+      #   false
+      # end
+      translations_installed = true
 
       unless defined?(::Rake::SprocketsTask)
         table_existence = ActiveRecord::Base.connection.table_exists? 'i18n_admin_translations_sets'
@@ -22,9 +24,9 @@ module I18nAdmin
     end
 
     initializer 'i18n_admin.add_assets_to_precompilation', group: :all do |app|
-      app.config.assets.precompile += %w(
+      app.config.assets.precompile += %w[
         'i18n_admin/spinner.gif'
-      )
+      ]
     end
   end
 end
